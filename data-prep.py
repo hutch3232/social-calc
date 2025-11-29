59a2ebcb43734842936a5b420a88e2d5a7f02002
hutch3232 <donnelpa@gmail.com>
2025-11-29 12:56:55 -0500

    use ruff, minor data-prep improvements

--- data-prep.py
+++ data-prep.py
@@ -7,17 +7,15 @@ for i in range(1, 19):
         df.select("column_1", "column_3")
         .slice(3)
         .rename({"column_1": "Age", "column_3": "NumSurvivors"})
-        .with_columns(pl.col("Age").str.split("–").list.first())
+        .filter(pl.col("NumSurvivors").is_not_null())
+        .with_columns(pl.col("Age").str.split("–").list.first())  # noqa: RUF001
+        .with_columns(
+            pl.when(pl.col("Age").str.starts_with("100"))
+            .then(pl.lit("100"))
+            .otherwise(pl.col("Age"))
+            .cast(pl.Int8)
+            .alias("Age"),
+        )
     )
 
-    df = df.filter(pl.col("NumSurvivors").is_not_null()).with_columns(
-        pl.when(pl.col("Age").str.starts_with("100"))
-        .then(pl.lit("100"))
-        .otherwise(pl.col("Age"))
-        .cast(pl.Int8)
-        .alias("Age"),
-    )
-    
-    df.write_csv(
-        f"data/{tbl_nm}.csv"
-    )
+    df.write_csv(f"data/{tbl_nm}.csv")
